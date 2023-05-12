146d963ad2b85ddde1db5383d08a07ce02ba48fb
alexandredchaves <xandechaves@gmail.com>
2023-05-12 11:20:13 -0300

    Get name by id

--- list/list/src/main/java/com/GameProjeto/list/GameController.java
+++ list/list/src/main/java/com/GameProjeto/list/GameController.java
@@ -2,6 +2,7 @@ package com.GameProjeto.list;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,12 @@ import java.util.List;
 public class GameController {
     @Autowired
     private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public Games findById(@PathVariable Long id) {
+        Games result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
     public List<MiniGame> findAll() {
--- list/list/src/main/java/com/GameProjeto/list/GameService.java
+++ list/list/src/main/java/com/GameProjeto/list/GameService.java
@@ -2,6 +2,7 @@ package com.GameProjeto.list;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,6 +11,13 @@ import java.util.List;
 public class GameService {
     @Autowired
     private GameRepository gameRepository;
+    @Transactional(readOnly = true)
+    public Games findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        Games games = new Games(result);
+        return games;
+    }
+    @Transactional(readOnly = true)
     public List<MiniGame> findAll() {
         List<Game> result = gameRepository.findAll();
         List<MiniGame> mini = result.stream().map(x -> new MiniGame(x)).toList();
